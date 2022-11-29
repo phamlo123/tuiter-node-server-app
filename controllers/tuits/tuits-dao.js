c854660965dba27cbf259269749c9f9f98ed594d
phamlo123 <pham.lo@northeastern.edu>
2022-11-29 13:52:32 -0500

    more

--- controllers/tuits/tuits-dao.js
+++ controllers/tuits/tuits-dao.js
@@ -2,10 +2,8 @@ import tuitsModel from './tuits-model.js';
 export const findTuits = () => tuitsModel.find();
 export const createTuit = (tuit) => tuitsModel.create(tuit);
 export const deleteTuit = (tid) => {
-    tid.trim()
     tuitsModel.deleteOne({_id: tid});
 }
 export const updateTuit = (tid, tuit) => {
-    tid.trim()
     tuitsModel.updateOne({_id: tid}, {$set: tuit})
 }
